2af429e1845c25931c32514da099380ef91aeff3
Hunter Elbourn <elbournhr@beloit.edu>
2015-05-01 02:45:25 -0500

    closes the db connection

--- public/added.php
+++ public/added.php
@@ -51,9 +51,11 @@ $query = "insert into CAccounts Values ('".$AID."', '".$CID."', '".$CID."', '".$
 $db->query($query);
 $query = "insert into Contact values (".$AID.", ".$CID.");";
 $db->query($query);
+
+mysqli_close(db); //close the opened db connection
 //we should probably add a check to make sure there were no conflicts updating the db and then route to the next page accordingly
 header("Location: success.php"); 
-
+exit; // just to make sure the header redirects and then we GTFO
 ?>
 
 
