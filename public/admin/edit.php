473e11dafbb1542a2553c9ce000b0adf2f5ba603
joepeacock001 <peacockjs@beloit.edu>
2015-05-03 22:04:16 -0500

    intermidiate commit- so that Hunter can test what we have

---
+++ public/admin/edit.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html >
+<style>
+table, th , td  {
+  border: 1px solid grey;
+  border-collapse: collapse;
+  padding: 5px;
+}
+table tr:nth-child(odd) {
+  background-color: #f1f1f1;
+}
+table tr:nth-child(even) {
+  background-color: #ffffff;
+}
+</style>
+<script src= "http://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.min.js"></script>
+<body>
+ 
+<div ng-app="myApp" ng-controller="customersCtrl">
+
+<form>
