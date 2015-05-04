ae40b6b3fa88f265b3779bb88f3592029f255a44
joepeacock001 <peacockjs@beloit.edu>
2015-05-03 22:49:32 -0500

    I think edit.php is done. I guess we'll see

--- public/admin/edit.php
+++ public/admin/edit.php
@@ -14,8 +14,69 @@ table tr:nth-child(even) {
 }
 </style>
 <script src= "http://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.min.js"></script>
+
+<?php
+
+if (isset($_GET['id'])) 
+	{ $ID=$db->real_escape_string(trim($_GET['id'])); }
+else 
+	{ $ID = ''; }
+
+$update_url = "update.php?id=".$ID;
+$np_url = "new_person.php?id=".$ID;
+
+
+?>
+
 <body>
  
 <div ng-app="myApp" ng-controller="customersCtrl">
 
-<form>
+<form method="GET" action=<?php echo $update_url; ?>>
+<label for="query">Organization Name:</label>
+    <input type="text" class="form-control" name="O_Name" value="{{names.O_Name}}"></br>
+    <label for="query">Location:</label>
+<input type="text" class="form-control" name="Loc" value="{{names.O_Name}}"></br>
+    <label for="query">Status:</label>
+<input type="text" class="form-control" name="Stat" value="{{names.O_Name}}"></br>
+</br></br>
+<div ng-repeat "x in names">
+<label for="query">Name:</label>
+<input type="text" class="form-control" name="name" value="{{ x.Name }}">
+<label for="query">Email:</label>
+<input type="text" class="form-control" name="email" value="{{ x.Email }}">
+<label for="query">Phone:</label>
+<input type="text" class="form-control" name="phone" value="{{ x.Phone }}">
+<label for="query">Assign Admin:</label>
+<input type="text" class="form-control" name="admin" {{x.Admin ? checked : unchecked}}>
+<label for="query">Assign Manager:</label>
+<input type="text" class="form-control" name="Manager" {{x.Manager ? checked : unchecked}}>
+<label for="query">Assign Contact:</label>
+<input type="text" class="form-control" name="Contact" {{x.Contact ? checked : unchecked}}>
+</div>
+</form>
+<form method="GET" action=<?php echo $np_url; ?>>
+<label for="query">Name:</label>
+<input type="text" class="form-control" name="name">
+<label for="query">Email:</label>
+<input type="text" class="form-control" name="email">
+<label for="query">Phone:</label>
+<input type="text" class="form-control" name="phone">
+<label for="query">Assign Admin:</label>
+<input type="text" class="form-control" name="admin" >
+<label for="query">Assign Manager:</label>
+<input type="text" class="form-control" name="Manager">
+<label for="query">Assign Contact:</label>
+<input type="text" class="form-control" name="Contact">
+
+</form>
+
+</div>
+
+<script>
+var app = angular.module('myApp', []);
+app.controller('customersCtrl', function($scope, $http) {
+   $http.get("editable.php")
+   .success(function (response) {$scope.names = response.records;});
+});
+</script>
