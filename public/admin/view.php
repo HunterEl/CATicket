9352cf1c82b4f9777a8642e2d6968d8195e5b9be
joepeacock001 <peacockjs@beloit.edu>
2015-05-05 18:45:23 -0500

    added a seperate view page to go with the edit page. Next step: Make the submits do something

--- public/admin/view.php
+++ public/admin/view.php
@@ -14,7 +14,7 @@ if (isset($_GET['id']))
 	{ $ID=$db->real_escape_string(trim($_GET['id'])); }
 else 
 	{ $ID = ''; }
-$update_url = "update.php?id=".$ID;
+$edit_url = "edit.php?id=".$ID;
 $np_url = "new_person.php?id=".$ID;
 
 $q1 = "select * from CAccounts where id=".$ID.";";
@@ -29,13 +29,13 @@ $row1 = $res1->fetch_assoc();
  
 <div ng-app="myApp" ng-controller="customersCtrl">
 
-<form method="GET" action=<?php echo $update_url; ?>>
+<form method="POST"  action=<?php echo $edit_url; ?>>
 <label for="query">Organization Name:</label>
-    <input type="text" class="form-control" name="O_Name" value="<?php echo $row1['O_Name']; ?>"></br>
+    <input type="text" class="form-control" name="O_Name" value="<?php echo $row1['O_Name']; ?>" readonly></br>
     <label for="query">Location:</label>
-<input type="text" class="form-control" name="Loc" value="<?php echo $row1['Location']; ?>"></br>
+<input type="text" class="form-control" name="Loc" value="<?php echo $row1['Location']; ?>" readonly></br>
     <label for="query">Status:</label>
-<input type="text" class="form-control" name="Stat" value="<?php echo $row1['status']; ?>"></br>
+<input type="text" class="form-control" name="Stat" value="<?php echo $row1['status']; ?>" readonly></br>
 </br></br>
 <?php 
 
@@ -53,24 +53,25 @@ for ($i=0;$i<$res2->num_rows;$i++)
 
 echo "
 <label for='query'>Name:</label>
-<input type='text' class='form-control' name='name' value='".$row2['cName']."'>
+<input type='text' class='form-control' name='name' value='".$row2['cName']."' readonly>
 <label for='query'>Email:</label>
-<input type='text' class='form-control' name='email' value='".$row2['Email']."'>
+<input type='text' class='form-control' name='email' value='".$row2['Email']."' readonly>
 <label for='query'>Phone:</label>
-<input type='text' class='form-control' name='phone' value='".$row2['Phone']."'>
+<input type='text' class='form-control' name='phone' value='".$row2['Phone']."' readonly>
 <label for='query'>Assign Admin:</label>
-<input type='checkbox' class='form-control' name='admin' ".$admin.">
+<input type='checkbox' class='form-control' name='admin' ".$admin." disabled>
 <label for='query'>Assign Manager:</label>
-<input type='checkbox' class='form-control' name='Manager' ".$man.">
+<input type='checkbox' class='form-control' name='Manager' ".$man." disabled>
 <label for='query'>Assign Contact:</label>
-<input type='checkbox' class='form-control' name='Contact' ".$contact.">
+<input type='checkbox' class='form-control' name='Contact' ".$contact." disabled>
 
 
 ";
 }
-echo "<button type='submit'>Submit Modifications</button></form>";
+echo "<button type='submit'>Edit</button></form>";
 
 echo "<h4>Caution! Each organization can only have one Admin and one Manager. Do not add a new Admin or Manager without removing the old one</h4>";
+echo "</br></br></br><h2> Add a new associated contact</h2>";
 ?>
 
 <form method="GET" action=<?php echo $np_url; ?>>
@@ -81,11 +82,11 @@ echo "<h4>Caution! Each organization can only have one Admin and one Manager. Do
 <label for="query">Phone:</label>
 <input type="text" class="form-control" name="phone">
 <label for="query">Assign Admin:</label>
-<input type="text" class="form-control" name="admin" >
+<input type="checkbox" class="form-control" name="admin" >
 <label for="query">Assign Manager:</label>
-<input type="text" class="form-control" name="Manager">
+<input type="checkbox" class="form-control" name="Manager">
 <label for="query">Assign Contact:</label>
-<input type="text" class="form-control" name="Contact">
+<input type="checkbox" class="form-control" name="Contact">
 <button type='submit'>Submit</button>
 </form>
 
